ba8eb0ccc7022c90ba26bbfb5a635b0472757979
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2022-08-25 03:54:46 -0400

    clear screen and for loop for seconds is working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
+/*
+ * Seconds Project
+ * Display seconds on screen as 2 digits
+ */
+
 #include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Declarations
+    int seconds = 0;
+
+    // Output
+    std::cout << setw(2) << setfill('0') << seconds << std::endl;
+    system("clear");
+
+
+    // Loop
+
+    for (int i = 0; i < 60; i++) {
+        seconds = i;
+        std::cout << setw(2) << setfill('0') << seconds << std::endl;
+        std::this_thread::sleep_for(1000ms);
+        system("clear");
+    }
+
     return 0;
+
+
 }
